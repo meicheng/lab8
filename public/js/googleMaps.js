aab0fb6c6f92aa41826761112a6aa4bde253b382
Helen Cheng <meicheng96@gmail.com>
2018-03-01 10:48:13 -0800

    Maps fixed!

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -2,7 +2,7 @@ function initMap() {
 	// add your code here
     var ucsd_ltling = {lat:32.880, lng:-117.236};
 
-	var map = new google.maps.Map(documnt.getElementById('map'), {
+	var map = new google.maps.Map(document.getElementById('map'), {
 		center: ucsd_ltling,
 		zoom: 15
 	});
