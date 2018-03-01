4d9fd04146eeed0671fd79f5ca0c510f366287c7
Helen Cheng <meicheng96@gmail.com>
2018-03-01 10:29:54 -0800

    maps

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,5 +1,7 @@
 function initMap() {
 	// add your code here
+    var ucsd_ltling = {lat:32.880, lng:-117.236};
+
 	var map = new google.maps.Map(documnt.getElementById('map'), {
 		center: used_ltling,
 		zoom: 15
