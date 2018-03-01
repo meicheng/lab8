9988cc71b49b52b74eb075864e3630961ac9abbe
Helen Cheng <meicheng96@gmail.com>
2018-03-01 10:23:43 -0800

    first stuff

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	var map = new google.maps.Map(documnt.getElementById('map'), {
+		center: used_ltling,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltling,
+		map: map,
+		title: "UCSD"
+	});
 }
